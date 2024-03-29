c188a0026cfbf50fff137486165974598d6d32ef
owen970517 <tamia123123@naver.com>
2024-03-29 17:12:08 +0900

    refactor: useMutation을 사용하여 input 코드 개선

--- src/common/UserInput.tsx
+++ src/common/UserInput.tsx
@@ -11,61 +11,67 @@ import LatestSearched from './LatestSearched';
 import useKeyboard from '../hooks/useKeyboard';
 import { fetchUserId } from '../apis/getOuid';
 import { RootState } from '../store/store';
+import { useMutation } from '@tanstack/react-query';
 
 const UserInput = () => {
   const dispatch = useDispatch();
-  const {isFocus} = useSelector((state:RootState) => state.matches)
+  const { isFocus } = useSelector((state: RootState) => state.matches);
   const { register, handleSubmit, watch, setValue } = useForm<IForm>();
-  const user = watch('user');
   const nav = useNavigate();
-  const prevSearched:string[] = JSON.parse(localStorage.getItem('searched') || '[]')
-  const {keyBoardIdx,onKeydown,initIndex} = useKeyboard(prevSearched)
+  const user = watch('user');
+  const prevSearched: string[] = JSON.parse(localStorage.getItem('searched') || '[]');
+  const { keyBoardIdx, onKeydown, initIndex } = useKeyboard(prevSearched);
 
-  const onSubmit = async () => {
-    const ouid = await fetchUserId(user);
-    dispatch(userActions.setOuid(ouid));
-    dispatch(matchActions.initState());
-    nav(`/search?nickname=${user}`);
-    setValue('user', '');
-    initIndex();
-    const updatedData = prevSearched.filter((data:string) => data !== user);
-    updatedData.unshift(user);
-    if (updatedData.length > 5) {
-      updatedData.pop();
+  const mutation = useMutation({
+    mutationKey : ['nickname',user],
+    mutationFn : fetchUserId,
+    onSuccess : (ouid) => {
+      dispatch(userActions.setOuid(ouid));
+      dispatch(matchActions.initState());
+      dispatch(matchActions.setIsLoadingInit(true));
+      nav(`/search?nickname=${user}`);
+      setValue('user', '');
+      initIndex();
+      const updatedData = prevSearched.filter((data: string) => data !== user);
+      updatedData.unshift(user);
+      if (updatedData.length > 5) {
+        updatedData.pop();
+      }
+      localStorage.setItem('searched', JSON.stringify(updatedData));
     }
-    localStorage.setItem('searched', JSON.stringify(updatedData));
+  })
+
+  const onSubmit = async (data: IForm) => {
+    mutation.mutate(data.user)
   };
-  
+
   const handleInput = (e: React.KeyboardEvent) => {
-    onKeydown(e)
-  }
+    onKeydown(e);
+  };
+
   useEffect(() => {
     if (keyBoardIdx !== null) {
-      const selectedName = prevSearched[keyBoardIdx];
-      setValue('user', selectedName);
+      setValue('user', prevSearched[keyBoardIdx]);
     } else {
-      setValue('user' , '')
+      setValue('user', '');
     }
   }, [keyBoardIdx]);
 
   useEffect(() => {
-    if (!isFocus) {
-      initIndex();
-    }
     initIndex();
-  },[initIndex, isFocus])
-  
+  }, [initIndex, isFocus]);
+
   return (
     <SearchContainer>
       <SearchBar onSubmit={handleSubmit(onSubmit)}>
-        <input 
-          {...register('user', {required:true})} 
-          type='text' 
-          autoComplete='off' 
+        <input
+          {...register('user', { required: true })}
+          type='text'
+          autoComplete='off'
           placeholder='구단주 명을 입력하시오'
           onFocus={() => dispatch(matchActions.setIsFocus(true))}
           onBlur={() => dispatch(matchActions.setIsFocus(false))}
-          onKeyDown={(e) => handleInput(e)}
+          onKeyDown={handleInput}
         />
         <button type='submit'>
           <SearchIcon size={21}/>
@@ -73,8 +79,8 @@ const UserInput = () => {
       </SearchBar>
       {isFocus && <LatestSearched nowIdx={keyBoardIdx!}/>}
     </SearchContainer>
-  )
-}
+  );
+};
 const SearchContainer = styled.div`
   position: relative;
 `
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,23 @@ import App from './App';
 import {Provider} from 'react-redux';
 import { store } from './store/store';
 import './styles/global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={true}/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
