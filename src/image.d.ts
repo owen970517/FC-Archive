afe2ac1569ace7dcb20c5779f6a7b46f835101ad
owen970517 <tamia123123@naver.com>
2026-07-29 15:34:00 +0900

    feat: CSS/SCSS 모듈 import 타입 선언 추가

--- src/image.d.ts
+++ src/image.d.ts
@@ -3,3 +3,7 @@ declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
 declare module '*.svg';
+declare module '*.css';
+declare module '*.module.css';
+declare module '*.scss';
+declare module '*.module.scss';
